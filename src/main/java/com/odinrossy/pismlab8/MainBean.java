4ce08c447c0b1d254e857c1f88d62cd940872119
Gleb Remniov <remniov.work@gmail.com>
2019-05-17 10:11:19 +0300

    Task ready

--- src/main/java/com/odinrossy/pismlab8/MainBean.java
+++ src/main/java/com/odinrossy/pismlab8/MainBean.java
@@ -12,9 +12,20 @@ public class MainBean implements Serializable {
 
     private String name;
 
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
